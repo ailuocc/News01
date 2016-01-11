8caef33e95d1acb3f5bdfba545e34528fe85e91c
Mac C <mac-c@maccdemacbook-pro.local>
2016-01-11 20:39:47 +0800

    1.11   20.33 更新

---
+++ 1111/1111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1111
+//
+//  Created by Mac－C on 16/1/11.
+//  Copyright © 2016年 毕灿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1111/1111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1111
+//
+//  Created by Mac－C on 16/1/11.
+//  Copyright © 2016年 毕灿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
